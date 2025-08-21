831b5bd3c7cdd0f64261cdccda32acfc702bd1f0
constance-oq <constance.oq@gmail.com>
2025-08-21 20:23:24 +1000

    roll text editor now uses a TextEdit instead of a label

--- scripts/roll_text_edit.gd
+++ scripts/roll_text_edit.gd
@@ -1,46 +1,35 @@
-extends Label
+extends TextEdit
 class_name RollTextEdit
-# Okay, it's not REALLY a TextEdit...
-# But it behaves like one! Sort of.
 
 func get_roll_text():
-	#TODO: sanitize_text()
+	sanitize_text()
 	return text
-
-func parse_input( event: InputEvent ):
-	if event.is_action_pressed( "edit_d" ):
-		text += "d"
-	if event.is_action_pressed( "edit_separator" ):
-		text += "+"
-	if event.is_action_pressed( "edit_backspace" ) and text != "":
-		text = text.substr( 0, text.length() - 1 )
-	parse_input_numbers( event )
-
-func parse_input_numbers( event: InputEvent ):
-	if !( event is InputEventKey ): return
-	if !( event.is_pressed() ): return
 	
-	var number: int = 0
-	if ( event.keycode >= KEY_0 and event.keycode <= KEY_9 ):
-		number = event.keycode - KEY_0
-	elif ( event.keycode >= KEY_KP_0 and event.keycode <= KEY_KP_9 ):
-		number = event.keycode - KEY_KP_0
-	else:
-		return
-
-	text += str( number )
+func _on_text_changed():
+	sanitize_text()
 
 func sanitize_text():
-	# WIP function.
-	var final_text := ""
+	var final_text := text
+	var caret_pos: int = get_caret_column()
+	
 	var reggie: RegEx = RegEx.new()
-	reggie.compile( "[d0-9]" )
-	var match = reggie.search_all( text )
-	if !match: text = final_text; return
-	for i in range( 0, match.size() ):
-		final_text += match[i].get_string()
-	text = final_text
-	return
+	var matches: Array[RegExMatch]
+	
+	reggie.compile( "[=, ]" )
+	matches = reggie.search_all( final_text )
+	for match in matches:
+		final_text[ match.get_start() ] = "+"
+		
+	reggie.compile( "[d0-9\\-\\+]" )
+	matches = reggie.search_all( final_text )
+	final_text = ""
+	for match in matches:
+		final_text += match.get_string()
 
+	text = final_text
+	set_caret_column( caret_pos )
+	return
+	
 func _input( event: InputEvent ):
-	parse_input( event )
+	if event.is_action_pressed( "roll" ):
+		release_focus()
