221c5846b4fb1b5911607fa702eaaede1dd52df5
constance-oq <constance.oq@gmail.com>
2025-08-26 15:04:53 +1000

    feat: dice can subtract from score (e.g. -2d6)

--- score_counter.gd
+++ score_counter.gd
@@ -7,8 +7,11 @@ signal score_counted( score: int )
 func count_score( active_dice: Node ):
 	var total_score: int = 0
 	for group: DiceGroup in active_dice.get_children():
+		var group_score: int = 0
 		for die: Die in group.get_children():
 			var die_score = die.get_score()
-			total_score += die_score
+			group_score += die_score
+		if group.subtract: group_score *= -1
+		total_score += group_score
 	
 	score_counted.emit( total_score )
