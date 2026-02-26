d1f755c9e4620e67da1b74197b2ce34c1dab978d
constance-oq <constance.oq@gmail.com>
2026-02-27 10:41:35 +1100

    graphics: MORE typewriter buttons

--- gui/elements/show_die_scores_button.gd
+++ gui/elements/show_die_scores_button.gd
@@ -1,10 +1,17 @@
 extends Button
 
+# Variable
 var shown: bool = false
+
+# Configurable
 @export var shown_icon: Texture2D
 @export var hidden_icon: Texture2D
 
+# References
+@onready var texture_rect = $TypewriterButton/TextureRect/TextureRect2
+# Remembering of course that typewriterbutton reparents TextureRect2.
+
 func _pressed():
 	shown = !shown
-	if shown: icon = shown_icon
-	else: icon = hidden_icon
+	if shown: texture_rect.texture = shown_icon
+	else: texture_rect.texture = hidden_icon
